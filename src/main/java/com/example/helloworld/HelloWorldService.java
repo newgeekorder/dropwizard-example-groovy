3aeea105cafa6b3a9ed8c8735d39a25161595c4a
Adam Jordens <adam@jordens.org>
2012-07-08 22:51:46 -0700

    Added GroovyHelloWorldResource, and necessary pom changes to work in IntelliJ.

--- src/main/java/com/example/helloworld/HelloWorldService.java
+++ src/main/java/com/example/helloworld/HelloWorldService.java
@@ -7,16 +7,15 @@ import com.example.helloworld.core.Template;
 import com.example.helloworld.core.User;
 import com.example.helloworld.db.PeopleDAO;
 import com.example.helloworld.health.TemplateHealthCheck;
-import com.example.helloworld.resources.HelloWorldResource;
-import com.example.helloworld.resources.PeopleResource;
-import com.example.helloworld.resources.PersonResource;
-import com.example.helloworld.resources.ProtectedResource;
+import com.example.helloworld.resources.*;
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.auth.basic.BasicAuthProvider;
 import com.yammer.dropwizard.bundles.AssetsBundle;
 import com.yammer.dropwizard.config.Environment;
 import com.yammer.dropwizard.db.Database;
 import com.yammer.dropwizard.db.DatabaseFactory;
+
+import javax.ws.rs.Path;
 
 public class HelloWorldService extends Service<HelloWorldConfiguration> {
     public static void main(String[] args) throws Exception {
@@ -45,10 +44,10 @@ public class HelloWorldService extends Service<HelloWorldConfiguration> {
 
         environment.addHealthCheck(new TemplateHealthCheck(template));
         environment.addResource(new HelloWorldResource(template));
+        environment.addResource(new GroovyHelloWorldResource(template));
         environment.addResource(new ProtectedResource());
 
         environment.addResource(new PeopleResource(peopleDAO));
         environment.addResource(new PersonResource(peopleDAO));
     }
-
 }
